5a9a3b85d90fd3c2de913a09d424379a757d9cd2
Rajat Roy <rajatroy.dev@gmail.com>
2021-12-22 22:23:29 +0530

    Java: Multiple inheritance using interface

---
+++ java/Inheritance/MultipleInheritance.java
@@ -0,0 +1,27 @@
+package Inheritance;
+
+interface First {
+    default void print() {
+        System.out.println("First interface: print()");
+    }
+}
+
+interface Second {
+    default void print() {
+        System.out.println("Second interface: print()");
+    }
+}
+
+class Child implements First, Second {
+    public void print() {
+        First.super.print();
+        Second.super.print();
+    }
+}
+
+public class MultipleInheritance {
+    public static void main(String[] args) {
+        Child child = new Child();
+        child.print();
+    }
+}
