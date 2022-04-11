fe8d66111c6913c84470ab2f887210ec6ec48cda
Rajat Roy <rajatroy.dev@gmail.com>
2022-04-11 22:51:54 +0530

    JS: Implicit coercion

---
+++ javascript/implicit-coercion.js
@@ -0,0 +1,23 @@
+let a = "1";
+let b = 1;
+
+console.log(a - b);
+console.log(typeof (a - b));
+console.log(a + b);
+console.log(typeof (a + b));
+
+let boolA = "";
+let boolB = "1";
+
+if (boolA)
+    console.log("boolA");
+if (boolB)
+    console.log("boolB");
+
+boolA = 0;
+boolB = 2;
+
+if (boolA)
+    console.log("boolA0");
+if (boolB)
+    console.log("boolB2")
