b0108f6e55543c5886b2a0d740641d9c623f7dbc
Rajat Roy <rajatroy.dev@gmail.com>
2022-04-12 23:00:32 +0530

    JS: YDKJS - Closures 1

---
+++ javascript/ydkjs/closure-1.js
@@ -0,0 +1,16 @@
+function foo() {
+    var a = 2; // this is defined inside closure -> foo
+
+    function baz() {
+        // function buz is part of closure -> foo
+        // so, it will be able to access a
+        console.log(a); // 2
+    }
+
+    bar(baz);
+}
+
+function bar(fn) {
+    // this is inside closure -> bar
+    fn(); // this is run as part of closure -> bar
+}
