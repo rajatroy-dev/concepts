46b32bcb27c26b00ac92a314442eb833325c6c21
Rajat Roy <rajatroy.dev@gmail.com>
2022-06-28 21:35:40 +0530

    Android: DataBinding Initial Understanding

---
+++ android/DataBinding/app/src/main/java/in/rajatroy/databinding/SecondFragment.kt
@@ -0,0 +1,52 @@
+package `in`.rajatroy.databinding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import `in`.rajatroy.databinding.databinding.FragmentSecondBinding
+
+/**
+ * A simple [Fragment] subclass as the second destination in the navigation.
+ */
+class SecondFragment : Fragment() {
+
+    private var _binding: FragmentSecondBinding? = null
+
+    // This property is only valid between onCreateView and
+    // onDestroyView.
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+
+        _binding = FragmentSecondBinding.inflate(inflater, container, false)
+        return binding.root
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.secondFragmentHelloButton.setOnClickListener {
+            greet()
+        }
+
+        binding.buttonSecond.setOnClickListener {
+            findNavController().navigate(R.id.action_SecondFragment_to_FirstFragment)
+        }
+    }
+
+    private fun greet() {
+        binding.secondFragmentGreetings.text = "Hello! " + binding.secondFragmentNameInput.text
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
